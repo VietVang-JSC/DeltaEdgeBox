0e8e1b9c5885a50cab1070dad771e84a3bdc3d70
Tran Thien Nhan <nhan_tt@vietvang.net>
2026-06-25 06:36:44 +0700

    audit: fix null guard cho ['products'] trong invoice templates

--- resources/views/invoice/template_invoice_vi_58.blade.php
+++ resources/views/invoice/template_invoice_vi_58.blade.php
@@ -271,14 +271,15 @@
 
                     @php
                         $productExtra = [];
-                        $itemVat = $item['products']['vat']/100;
+                        $products = $item['products'] ?? [];
+                        $itemVat = ($products['vat'] ?? 0) / 100;
                         $totalItemPrice = ($is_tax_included == 1 && ($seniorDiscount ?? 0) > 0) ? round($item['total_price']/(1+$itemVat)) : $item['total_price'];
                         $itemQuantity = $item['quantity'];
                         $itemPrice = $itemQuantity > 0 ? $totalItemPrice / $itemQuantity : 0;
                         if(!empty($item['product_extra'])){
                             $productExtra = json_decode($item['product_extra'], true);
                         }
-                        $productTile = $item['products']['title'] ?? '';
+                        $productTile = $products['title'] ?? '';
                         $subTotal += $totalItemPrice;
                     @endphp
 
