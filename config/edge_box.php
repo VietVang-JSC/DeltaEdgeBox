6ec32cbf6cb60f484246ce7981265bf7a3abbe6c
Tran Thien Nhan <nhan_tt@vietvang.net>
2026-07-06 21:51:07 +0700

    chore: change default timezone to Asia/Manila

--- config/edge_box.php
+++ config/edge_box.php
@@ -6,5 +6,5 @@ return [
     'store_id' => env('STORE_ID'),
     'sqlite_lock_ttl' => 600,
     'sync_interval' => env('SYNC_INTERVAL', '1m'),
-    'timezone' => env('APP_TIMEZONE', 'Asia/Ho_Chi_Minh'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Manila'),
 ];
