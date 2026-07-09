ef7294c89935052a138a69b3d72a9e7cdb882ffc
ngvqphuc2610 <nguyenvqphuc@gmail.com>
2026-07-09 15:32:09 +0700

    feat: update item input senior discount for payment controller and check timezone

--- resources/views/invoice/template_invoice_jp_80.blade.php
+++ resources/views/invoice/template_invoice_jp_80.blade.php
@@ -8,18 +8,8 @@
     <style>
         @page { size: auto; margin: 0mm; }
 
-        @font-face {
-            font-family: 'NotoSerifJP-SemiBold';
-            src: url("{{ storage_path('fonts/NotoSerifJP-SemiBold.ttf') }}") format('truetype');
-        }
-
-        @font-face {
-            font-family: 'NotoSansJP-Bold';
-            src: url("{{ storage_path('fonts/NotoSansJP-Bold.ttf') }}") format('truetype');
-        }
-
         body {
-            font-family: 'NotoSerifJP-SemiBold', serif;
+            font-family: Meiryo !important;
             background-color: #f9f9f9;
             margin: 0;
             padding: 0;
