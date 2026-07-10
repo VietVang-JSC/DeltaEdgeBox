505af7b811083aa3c6156d8cd6d235cf394efe57
ngvqphuc2610 <nguyenvqphuc@gmail.com>
2026-07-10 15:32:49 +0700

    feat: change fomat of printer kitchen

--- resources/views/kitchen/cook_template_print_all_80.blade.php
+++ resources/views/kitchen/cook_template_print_all_80.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <style>
         body {
-            font-family: DejaVu Sans !important;
+            font-family: 'notosansjp-regular', sans-serif !important;
             width: 80mm;
             margin: 0 auto;
         }
