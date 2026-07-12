f540737cedbfde6b75a85c2f69a7feda73389b6a
Tran Thien Nhan <nhan_tt@vietvang.net>
2026-07-12 20:22:59 +0700

    fix: add id to User \ so sync preserves cloud user IDs

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
         'phone',
        // 'email',
         'store_id',
